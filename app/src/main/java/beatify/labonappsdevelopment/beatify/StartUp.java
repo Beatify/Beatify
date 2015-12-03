02bd5d8f063f46ba620daf0085097fcda1477049
thuetter <thomas.huetter@stud.sbg.ac.at>
2015-12-03 21:17:07 +0100

    create global userData, userPlaylists and userPlaylistsTracks with asynchronous requests

--- app/src/main/java/beatify/labonappsdevelopment/beatify/StartUp.java
+++ app/src/main/java/beatify/labonappsdevelopment/beatify/StartUp.java
@@ -1,5 +1,6 @@
 package beatify.labonappsdevelopment.beatify;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -50,8 +52,14 @@ public class StartUp extends AppCompatActivity implements
     private static final int REQUEST_CODE = 1337;
 
     private Player mPlayer;
-    private final String USER_AGENT = "Mozilla/5.0";
-    private UserPrivate uData;
+    //needed for request of the kaees spotify wrapper
+    SpotifyApi api;
+    SpotifyService spotify;
+
+    //store spotify data
+    private UserPrivate userData;
+    private List<PlaylistSimple> userPlaylists;
+    private HashMap<String, List<PlaylistTrack>> userPlaylistsTracks = new HashMap<String, List<PlaylistTrack>>();
 
 
     @Override
@@ -76,55 +84,56 @@ public class StartUp extends AppCompatActivity implements
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
             if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-                SpotifyApi api = new SpotifyApi();
+                api = new SpotifyApi();
                 api.setAccessToken(response.getAccessToken());
-                SpotifyService spotify = api.getService();
+                spotify = api.getService();
 
-                //get UserID
+                //new getUserID().execute();
+
+                //get user_id
                 spotify.getMe(new Callback<UserPrivate>() {
                     @Override
-                    public void success(UserPrivate userPrivate, retrofit.client.Response response) {
+                    public void success(final UserPrivate userPrivate, retrofit.client.Response response) {
                         Log.d("User success", userPrivate.id);
-                        uData = userPrivate;
+                        userData = userPrivate;
+                        //get playlists
+                        spotify.getPlaylists(userPrivate.id, new Callback<Pager<PlaylistSimple>>() {
+                            @Override
+                            public void success(Pager<PlaylistSimple> playlistSimplePager, Response response) {
+                                List<PlaylistSimple> playlists = playlistSimplePager.items;
+                                userPlaylists = playlists;
+                                for (PlaylistSimple p : playlists) {
+                                    //Log.e("TEST", p.name + " - " + p.id);
+                                    //get tracks of playlists
+                                    spotify.getPlaylistTracks(userPrivate.id, p.id, new Callback<Pager<PlaylistTrack>>() {
+                                        @Override
+                                        public void success(Pager<PlaylistTrack> playlistTrackPager, Response response) {
+                                            //Log.e("TEST", "GOT the tracks in playlist");
+                                            List<PlaylistTrack> tracks = playlistTrackPager.items;
+                                            userPlaylistsTracks.put(response.getUrl().split("/")[7], tracks);
+                                            //for (PlaylistTrack pt : tracks) {
+                                                //Log.e("TEST", pt.track.name + " - " + pt.track.artists.get(0).name + " - " + pt.track.id);
+                                            //}
+                                        }
+
+                                        @Override
+                                        public void failure(RetrofitError error) {
+                                            Log.e("TEST", "Could not get playlist tracks");
+                                        }
+                                    });
+                                }
+                            }
+
+                            @Override
+                            public void failure(RetrofitError error) {
+                                Log.e("TEST", "Could not get playlists");
+                            }
+                        });
                     }
 
                     @Override
                     public void failure(RetrofitError error) {
-                        //TODO: Errorhandling
-                    }
-                });
-
-                //get all playlists: should replace huetterth with UserID from above
-                spotify.getPlaylists("huetterth", new Callback<Pager<PlaylistSimple>>() {
-                    @Override
-                    public void success(Pager<PlaylistSimple> playlistSimplePager, Response response) {
-                        Log.d("TEST", "Got the playlists");
-                        List<PlaylistSimple> playlists = playlistSimplePager.items;
-                        for (PlaylistSimple p : playlists) {
-                            Log.e("TEST", p.name + " - " + p.id);
-                        }
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-                        Log.e("TEST", "Could not get playlists");
-                    }
-                });
-
-                //get all songs from a playlist: again replace UserID and do it for all playlists
-                spotify.getPlaylistTracks("huetterth", "35cPdE04iQQgztYxNyx2mC", new Callback<Pager<PlaylistTrack>>() {
-                    @Override
-                    public void success(Pager<PlaylistTrack> playlistTrackPager, Response response) {
-                        Log.e("TEST", "GOT the tracks in playlist");
-                        List<PlaylistTrack> items = playlistTrackPager.items;
-                        for (PlaylistTrack pt : items) {
-                            Log.e("TEST", pt.track.name + " - " + pt.track.artists.get(0).name + " - " + pt.track.id);
-                        }
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-                        Log.e("TEST", "Could not get playlist tracks");
+                        Log.e("TEST", "Could not get userdata");
                     }
                 });
 
@@ -147,6 +156,101 @@ public class StartUp extends AppCompatActivity implements
             }
         }
     }
+
+/*
+    private class getUserID extends AsyncTask<Void, Void, UserPrivate> {
+
+        @Override
+        protected UserPrivate doInBackground(Void... params) {
+            Log.d("async:", "start");
+            try {
+                UserPrivate userPrivate = spotify.getMe();
+                return userPrivate;
+            } catch (RetrofitError error) {
+                SpotifyError spotifyError = SpotifyError.fromRetrofitError(error);
+                // handle error
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(UserPrivate up) {
+            Log.d("ID:", up.id);
+            userData = up;
+            new getAllPlaylists().execute(userData.id);
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+
+
+    private class getAllPlaylists extends AsyncTask<String, Void, Pager<PlaylistSimple>> {
+
+        @Override
+        protected Pager<PlaylistSimple> doInBackground(String... params) {
+            Log.d("async:", "start playlist");
+            try {
+                Pager<PlaylistSimple> ppls = spotify.getPlaylists(params[0]);
+                return ppls;
+            } catch (RetrofitError error) {
+                SpotifyError spotifyError = SpotifyError.fromRetrofitError(error);
+                // handle error
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Pager<PlaylistSimple> ppls) {
+            List<PlaylistSimple> plList = ppls.items;
+            userPlaylists = plList;
+            for (PlaylistSimple p: plList) {
+                Log.e("TEST", p.name + " - " + p.id);
+                //get infos for all playlists
+                new getPlaylistSongs().execute(userData.id, p.id);
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+
+    private class getPlaylistSongs extends AsyncTask<String, Void, Pager<PlaylistTrack>> {
+
+        @Override
+        protected Pager<PlaylistTrack> doInBackground(String... params) {
+            Log.d("async:", "start playlistsongs");
+            try {
+                Pager<PlaylistTrack> pplst = spotify.getPlaylistTracks(params[0], params[1]);
+                return pplst;
+            } catch (RetrofitError error) {
+                SpotifyError spotifyError = SpotifyError.fromRetrofitError(error);
+                // handle error
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Pager<PlaylistTrack> pplst) {
+            List<PlaylistTrack> pltList = pplst.items;
+            for (PlaylistTrack pt: pltList) {
+                Log.e("TEST", pt.track.name + " - " + pt.track.artists.get(0).name + " - " + pt.track.id);
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+*/
 
     @Override
     public void onLoggedIn() {
